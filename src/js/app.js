2b0d3a12224af0e820382d5c4dc23ae5ab251b03
Sam Granieri, Jr <sgranieri@thecitybase.com>
2023-03-20 12:13:56 -0500

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import { createRoot } from 'react-dom/client';
+
+function NavigationBar() {
+  return <h1>Hello from React!</h1>;
+}
+
+const domNode = document.getElementById('navigation');
+const root = createRoot(domNode);
+root.render(<NavigationBar />);
