788cb37a7af3d4a3f7d436d2a34cfc2aedd15829
Sam Granieri, Jr <sgranieri@thecitybase.com>
2023-03-22 08:48:02 -0500

    adding in react latest

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,30 @@
 import { createRoot } from 'react-dom/client'
+import { RichText } from '@graphcms/rich-text-react-renderer'
 
-function NavigationBar() {
-  return <h1>Hello from React!</h1>
+const content = {
+  children: [
+    {
+      type: 'paragraph',
+      children: [
+        {
+          bold: true,
+          text: 'Hello World!',
+        },
+      ],
+    },
+  ],
 }
 
+function App() {
+  return (
+    <RichText
+      content={content}
+      renderers={{
+        p: ({ children }) => <h1 className="text-white">{children}</h1>,
+      }}
+    />
+  )
+}
 const domNode = document.getElementById('navigation')
 const root = createRoot(domNode)
-root.render(<NavigationBar />)
+root.render(<App />)
